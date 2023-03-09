0e868ba2809f039bab38fe7e5d163d638bd7d655
abdallah <abdallahelmekawy6@gmail.com>
2023-03-09 03:28:14 +0200

    nested routes

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,13 +1,14 @@
-import { Link, link } from "react-router-dom";
+import { Outlet } from "react-router-dom";
+import Navbar from "../components/Navbar";
 
 function Home() {
   return (
-    <section className="section">
-      <h2>Home</h2>
-      <Link to="/about" className="btn">
-        About
-      </Link>
-    </section>
+    <>
+      <Navbar />
+      <section className="section">
+        <Outlet />
+      </section>
+    </>
   );
 }
 
--- src/pages/Products.js
+++ src/pages/Products.js
@@ -1,7 +1,12 @@
+import { Link } from "react-router-dom";
+
 function Products() {
   return (
     <section className="section">
       <h2>Products</h2>
+      <Link to="/" className="btn">
+        Back Home
+      </Link>
     </section>
   );
 }
